964798b6671b849dbde9818c519d643b672b6ac6
Ronny Bangsund <github@neuralshortcircuit.com>
2018-01-26 23:00:22 +0100

    Tidied up code and moved more into functions in other files.

--- main.go
+++ main.go
@@ -1,33 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"regexp"
-	"sort"
-	"strconv"
-	"strings"
-
-	"gopkg.in/src-d/go-git.v4/plumbing"
 
 	git "gopkg.in/src-d/go-git.v4"
 )
 
-func main() {
+func init() {
 	for _, x := range os.Args {
 		if x == "-h" {
 			showHelp()
 			os.Exit(0)
 		}
 	}
+}
 
+func main() {
 	repo, err := git.PlainOpen(".")
 	if err != nil {
 		if err == git.ErrRepositoryNotExists {
 			pr("No repository in the current path.")
 			os.Exit(1)
 		} else {
-			pr("Error: %s", err.Error())
+			pr("Error: %s.", err.Error())
 		}
 		os.Exit(2)
 	}
@@ -38,31 +33,21 @@ func main() {
 		os.Exit(2)
 	}
 
-	ref, err := repo.Tags()
+	tags, err := GetTags(repo)
 	if err != nil {
-		pr("Error: %s", err.Error())
+		pr("Error: %s.", err.Error())
 		os.Exit(2)
 	}
-
-	var tags []string
-	ref.ForEach(func(p *plumbing.Reference) error {
-		t := strings.Replace(p.Name().String(), "refs/tags/", "", 1)
-		tags = append(tags, t)
-		return nil
-	})
 
 	var last string
 	if len(tags) < 1 {
 		last = "v0.0.0"
 	} else {
-		sort.Strings(tags)
 		last = tags[len(tags)-1]
 	}
 
-	pattern := `(v*)(\d+)(\.)(\d+)(\.)(\d+)(.*)`
-	match, err := regexp.MatchString(pattern, last)
-	if !match {
-		pr("%s doesn't conform to a valid semantic version.", last)
+	if !isSemVer(last) {
+		pr("%s doesn't conform to a valid semantic version structure.", last)
 		os.Exit(1)
 	}
 
@@ -87,58 +72,5 @@ func main() {
 		os.Exit(2)
 	}
 
-	pr("New version is %s", version)
-}
-
-// BumpVersion takes a version string and increases a segment by 1, depending on the part argument.
-// major, ma: increase the first segment
-// minor, mi: increase the second segment
-// patch, p: increase the third (last) segment
-// If it starts with "v" it's preserved in the resulting version, to maintain consistency.
-func BumpVersion(ver, part string) (string, error) {
-	bump := ver
-	v := false
-	if strings.HasPrefix(bump, "v") {
-		v = true
-		bump = bump[1:]
-	}
-
-	a := strings.Split(bump, ".")
-	major, err := strconv.Atoi(a[0])
-	if err != nil {
-		return "", err
-	}
-
-	minor, err := strconv.Atoi(a[1])
-	if err != nil {
-		return "", err
-	}
-
-	patch, err := strconv.Atoi(a[2])
-	if err != nil {
-		return "", err
-	}
-
-	switch part {
-	case "major", "ma":
-		major++
-		minor = 0
-		patch = 0
-	case "minor", "mi":
-		minor++
-		patch = 0
-	case "patch", "p":
-		patch++
-	default:
-		pr("Unknown command. Bailing out.")
-	}
-
-	var version string
-	if v {
-		version = fmt.Sprintf("v%d.%d.%d", major, minor, patch)
-	} else {
-		version = fmt.Sprintf("%d.%d.%d", major, minor, patch)
-	}
-
-	return version, nil
+	pr("The new version is %s", version)
 }
